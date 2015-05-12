67a7d384920bbf336690796ed7dd20f50a18f1f9
Yukino Ikegami <yukino0131@me.com>
2015-05-13 02:01:48 +0900

    Refine codes

--- machinelearning/ftrl_proximal.py
+++ machinelearning/ftrl_proximal.py
@@ -25,34 +25,35 @@ class FTRLProximal:
         self.g_square_sum = np.zeros((k, n), dtype=np.float16)  # Σ glad**2
 
     def predict(self, x, w):
-        def sigmoid(z):
-            return 1.0 / (1 + np.exp(-z * self.gain))
-        return sigmoid(np.inner(x, w))
+        def sigmoid(z, gain):
+            return 1.0 / (1 + np.exp(-z * gain))
+        return sigmoid(np.inner(x, w), self.gain)
 
-    def _update_weight(self, cls, f):
-        z = self.z[cls][f]
+    def _update_weight(self, cls, i):
+        z = self.z[cls][i]
         if np.abs(z) > self.LAMBDA_ONE:
-            self.w[cls][f] -= self.eta[cls][f] * (z - (np.sign(z) * self.LAMBDA_ONE))
+            self.w[cls][i] -= self.eta[cls][i] * (z - (np.sign(z) * self.LAMBDA_ONE))
         else:
-            self.w[cls][f] = 0
+            self.w[cls][i] = 0
+
+    def train_one(self, t, feature):
+        for cls in range(self.k):
+            pred_result = self.predict(feature, self.w[cls])
+            glad = (pred_result - int(t == cls)) * feature
+            self.g_square_sum[cls] += glad**2
+            for i in range(self.n):
+                self.eta[cls][i] = self.alpha / (self.beta + np.sqrt(self.g_square_sum[cls][i]))
+                learning_rate = (1.0 / self.eta[cls][i]) - (1.0 / self.prev_eta[cls][i])
+                self.z[cls][i] += glad[i] - learning_rate * self.w[cls][i]
+                self._update_weight(cls, i)
+                self.prev_eta[cls][i] = self.eta[cls][i]
 
     def train(self, x):
-        data_order = list(range(len(x)))
-        for t in range(self.loop):
-            np.random.shuffle(data_order)
-            for d in data_order:
-                feature = x[d][1:]
-                t = x[d][0]
-                for cls in range(self.k):
-                    pred_result = self.predict(feature, self.w[cls])
-                    glad = (pred_result - int(t == cls)) * feature
-                    self.g_square_sum[cls] += glad**2
-                    for i in range(self.n):
-                        self.eta[cls][i] = self.alpha / (self.beta + np.sqrt(self.g_square_sum[cls][i]))
-                        learning_rate = (1.0 / self.eta[cls][i]) - (1.0 / self.prev_eta[cls][i])
-                        self.z[cls][i] += glad[i] - learning_rate * self.w[cls][i]
-                        self._update_weight(cls, i)
-                        self.prev_eta[cls][i] = self.eta[cls][i]
+        learning_order = list(range(len(x)))
+        for i in range(self.loop):
+            np.random.shuffle(learning_order)
+            for j in learning_order:
+                self.train_one(x[j][0], x[j][1:])
 
 
 if __name__ == '__main__':
