1a4d587c71b6d461043a97dcfb11e87ab2272f23
Yukino Ikegami <yukino0131@me.com>
2016-11-22 23:07:32 +0900

    update FTRL

--- machinelearning/ftrl_proximal/ftrl_proximal.py
+++ machinelearning/ftrl_proximal/ftrl_proximal.py
@@ -13,11 +13,11 @@ class FTRLProximal:
     http://static.googleusercontent.com/media/research.google.com/ja//pubs/archive/41159.pdf
     """
 
-    def __init__(self, k, n, i=10, alpha=0.05, beta=1.0):
+    def __init__(self, k, n, i=10, alpha=0.01, beta=1.0):
         self.k = k  # num of classes
         self.n = n  # num of features
         self.loop = i
-        self.LAMBDA_ONE = 0.2
+        self.l1 = 1.0
         self.alpha = alpha
         self.beta = beta
 
@@ -27,22 +27,22 @@ class FTRLProximal:
         self.z = np.zeros((k, n), dtype=np.float16)
         self.grad_square_sum = np.zeros((k, n), dtype=np.float16)  # Σ glad**2
 
-    def predict(self, x, w):
+    def predict(self, x):
         def softmax(x):
             e_x = np.exp(x - np.max(x))
             return e_x / e_x.sum()
-        return softmax(np.dot(w, x) + self.bias)
+        return softmax(np.dot(self.w, x) + self.bias)
 
     def _update_weight(self):
-        self.w[np.where(self.z <= self.LAMBDA_ONE)] = 0
-        indices = np.where(self.z > self.LAMBDA_ONE)
-        self.w[indices] += self.eta[indices] * (self.z[indices] - (np.sign(self.z[indices]) * self.LAMBDA_ONE))
+        self.w[np.where(self.z <= self.l1)] = 0
+        indices = np.where(self.z > self.l1)
+        self.w[indices] += self.eta[indices] * (self.z[indices] - (np.sign(self.z[indices]) * self.l1))
 
-    def train_one(self, y_idx, feature):
+    def train_one(self, feature, y):
         t = np.zeros((self.k, self.n))
-        t[y_idx] = feature
+        t[y] = feature
 
-        pred_result = self.predict(feature, self.w)
+        pred_result = self.predict(feature)
 
         self.bias -= np.mean(pred_result, axis=0)
 
@@ -54,28 +54,29 @@ class FTRLProximal:
         self._update_weight()
         self.prev_eta = self.eta
 
-    def train(self, x):
-        num_data = len(x)
+    def fit(self, X, y):
+        num_data = len(X)
         for i in range(self.loop):
             for j in np.random.permutation(num_data):
-                self.train_one(x[j][0], x[j][1:])
+                self.train_one(X[j], y[j])
 
 
 if __name__ == '__main__':
     X = np.array(
         (
-            (0, 1, 0, 0, 0),
-            (0, 1, 1, 0, 0),
-            (1, 0, 1, 0, 0),
-            (1, 0, 1, 1, 0),
-            (2, 0, 0, 1, 0),
-            (2, 0, 0, 1, 1),
+            (1, 0, 0, 0),
+            (1, 0, 0, 0),
+            (0, 1, 0, 0),
+            (0, 1, 0, 0),
+            (0, 0, 1, 0),
+            (0, 0, 1, 1),
         )
     )
+    y = np.array((0, 0, 1, 1, 2, 2))
 
     k = 3
-    ftrlp = FTRLProximal(k, 4, i=100, alpha=0.05)
-    ftrlp.train(X)
+    ftrlp = FTRLProximal(k, 4, i=1000, alpha=0.01)
+    ftrlp.fit(X, y)
     print('WEIGHT:')
     print(ftrlp.w)
     print('BIAS:')
@@ -83,8 +84,7 @@ if __name__ == '__main__':
     print('Z:')
     print(ftrlp.z)
     print('PREDICT:')
-    print(ftrlp.predict(np.array((1, 0, 0, 0)), ftrlp.w))
-    print(ftrlp.predict(np.array((0, 1, 0, 0)), ftrlp.w))
-    print(ftrlp.predict(np.array((0, 0, 1, 0)), ftrlp.w))
-    print(ftrlp.predict(np.array((0, 0, 0, 1)), ftrlp.w))
-    print(ftrlp.predict(np.array((1, 1, 1, 1)), ftrlp.w))
+    print(ftrlp.predict(np.array((1, 0, 0, 0))))
+    print(ftrlp.predict(np.array((0, 1, 0, 0))))
+    print(ftrlp.predict(np.array((0, 0, 1, 0))))
+    print(ftrlp.predict(np.array((0, 0, 0, 1))))
